ef18f27ddbe34b40b770804ee34cef647d1ac1ec
Archana Balachandran <80183110+Archana-balachandran@users.noreply.github.com>
2022-03-13 16:41:02 -0700

    additional input args name and autosave plot

--- util.py
+++ util.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-def plot_graph(episodes, rewards, algorithm):
+def plot_graph(episodes, rewards, algorithm,Addln_info):
     plt.plot(episodes, rewards)
     plt.xlabel('Episode')
     plt.ylabel('Rewards')
-    plt.title(algorithm + 'Plot')
-    plt.show()
+    plt.title(algorithm + '_Plot')
+    path='assets/'+algorithm+Addln_info+'.jpg'
+    plt.savefig(path)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
